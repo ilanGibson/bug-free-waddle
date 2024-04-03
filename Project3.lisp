1d8b2ca67877ce057248df20871bad2e716a041d
Ilan Gibson <ilanjgibson@gmail.com>
2024-04-03 16:28:19 -0600

    Finish TODO 1 running correctly

--- Project3.lisp
+++ Project3.lisp
@@ -5,12 +5,9 @@
 ;;  (set-member '(1 2) 1) => T
 ;;  (set-memebr '(1 2) 3) => NIL
 (defun set-member (set item)
-    (loop for i in set
-        when (= i item)
-        ;; do (format t "T")
-        do (progn (format t "T") (return))
-        end))
+    (cond
+        ((null set) NIL)
+        ((eq (car set) item) T)
+        (t (set-member (cdr set) item))))
 
-    ;; (format t "NIL"))
-
-(set-member '(1 2 3) 2)
+(format t "set-member: ~a~%" (set-member '(1 2 3) 4))
