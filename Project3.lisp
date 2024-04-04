84cea71e032041abff48f80eed70c195600e983b
Ilan Gibson <ilanjgibson@gmail.com>
2024-04-04 16:01:47 -0600

    Finish TODO 4 and 5 running correctly

--- Project3.lisp
+++ Project3.lisp
@@ -8,9 +8,9 @@
     (cond
         ((null set) NIL)
         ((eq (car set) item) T)
-        (t (set-member (cdr set) item))))
+        (T (set-member (cdr set) item))))
 
-;; (format t "set-member: ~a~%" (set-member '(1 2) 2))
+;; (format t "set-member: ~a~%" (set-member '(1 2) 1))
 
 
 ;; Return the union of set-1 and set-2.
@@ -42,16 +42,42 @@
         ((set-member set-2 (car set-1)) (cons (car set-1) (set-intersection (cdr set-1) set-2)))
         ((not(set-member set-2 (car set-1))) (set-intersection (cdr set-1) set-2))))
 
-(format t "set-intersection: ~a~%" (set-intersection '(1 2) '(2 4)))
+;; (format t "set-intersection: ~a~%" (set-intersection '(1 2) '(2 4)))
+
 
 ;; Return the difference of set-1 and set-2.
 ;; The result should contain no duplicates.
 ;; Assume set-1 contains no duplicates and set-2 contains no duplicates.
 ;; Examples:
 ;;   (set-diff '(1 2) '(2 4)) => '(1)
-;; (defun set-diff (set-1 set-2)
-;;     (cond
-;;         ((null set-1) NIL)
-;;         ((null set-2) set-1)
+(defun set-diff (set-1 set-2)
+    (cond
+        ((null set-1) NIL)
+        ((null set-2) set-1)
+        ((set-member set-2 (car set-1)) (set-diff (cdr set-1) set-2))
+        ((not(set-member set-2 (car set-1))) (cons (car set-1) (set-diff (cdr set-1) set-2)))))
 
+;; (format t "set-diff: ~a~%" (set-diff '(1 2) '(2 4)))
+
+
+;; Return the exclusive or of a and b
+;;
+;; Examples:
+;;  (boolean-xor t nil) => t
+;;  (boolean-xor nil nil) => nil
+(defun boolean-xor (a b)
+    (cond 
+        ((and a b) NIL)
+        ((or (and a b) (or a b)) T)))
+
+;; (format t "boolean-xor: ~a~%" (boolean-xor t nil))
+
+;; Return the implication of a and b
+;; Examples:
+;;  (boolean-implies t nil) => nil
+;;  (boolean-implies nil nil) => t
+;; (defun boolean-implies (a b)
+;;     (cond
+;;         ((and a b) T)
+;;         )
 ;; )
